9a40602cac68ed19fb7d99c9d06fd1c8225f62e3
Serge Babayan <serj.b.babayan@gmail.com>
2015-05-08 23:42:31 -0400

    Implemented Nod ring android demo code

--- app/src/main/java/dreamgeese/drivesafe/MainActivity.java
+++ app/src/main/java/dreamgeese/drivesafe/MainActivity.java
@@ -1,18 +1,93 @@
 package dreamgeese.drivesafe;
 
+import android.bluetooth.BluetoothDevice;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import net.openspatial.*;
 
 
 public class MainActivity extends ActionBarActivity {
+    public static final String TAG = "TargetEuler";
+    OpenSpatialService mOpenSpatialService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bindService(new Intent(this, OpenSpatialService.class), mOpenSpatialServiceConnection, BIND_AUTO_CREATE);
     }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unbindService(mOpenSpatialServiceConnection);
+    }
+
+    public void UpdateAngle(String ring, EulerAngle angle) {
+        String logline = ring + " " + Double.toString(angle.roll)
+                + " " + Double.toString(angle.pitch)
+                + " " + Double.toString(angle.yaw);
+        Log.d(TAG, logline);
+    }
+
+    private ServiceConnection mOpenSpatialServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mOpenSpatialService = ((OpenSpatialService.OpenSpatialServiceBinder)service).getService();
+            // CODE FROM STEP FIVE GOES HERE
+
+            mOpenSpatialService.getConnectedDevices();
+            mOpenSpatialService.initialize(TAG, new OpenSpatialService.OpenSpatialServiceCallback() {
+                @Override
+                public void deviceConnected(final BluetoothDevice bluetoothDevice) {
+                    try {
+                        mOpenSpatialService.registerForPose6DEvents(bluetoothDevice, new OpenSpatialEvent.EventListener() {
+                            @Override
+                            public void onEventReceived(OpenSpatialEvent openSpatialEvent) {
+                                String ringname = bluetoothDevice.getName();
+                                Pose6DEvent event = (Pose6DEvent)openSpatialEvent;
+                                EulerAngle angle = event.getEulerAngle();
+                                UpdateAngle(ringname, angle);
+                            }
+                        });
+                    } catch (OpenSpatialException e) {
+                        Log.e(TAG, "Could not register for Pose6D event " + e);
+                    }
+                }
+                @Override
+                public void deviceDisconnected(BluetoothDevice bluetoothDevice) {
+                }
+                @Override
+                public void buttonEventRegistrationResult(BluetoothDevice bluetoothDevice, int i) {
+                }
+                @Override
+                public void pointerEventRegistrationResult(BluetoothDevice bluetoothDevice, int i) {
+                }
+                @Override
+                public void pose6DEventRegistrationResult(BluetoothDevice bluetoothDevice, int i) {
+                }
+                @Override
+                public void gestureEventRegistrationResult(BluetoothDevice bluetoothDevice, int i) {
+                }
+                @Override
+                public void motion6DEventRegistrationResult(BluetoothDevice bluetoothDevice, int i) {
+                }
+            });
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mOpenSpatialService = null;
+        }
+    };
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,4 +110,5 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
