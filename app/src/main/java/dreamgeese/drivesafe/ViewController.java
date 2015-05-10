8c00256e1e15b13f3d84f568709cad57ba74eab4
Serge Babayan <serj.b.babayan@gmail.com>
2015-05-09 21:30:39 -0400

    Started to implement custom text field for phone number

--- app/src/main/java/dreamgeese/drivesafe/ViewController.java
+++ app/src/main/java/dreamgeese/drivesafe/ViewController.java
@@ -1,6 +1,7 @@
 package dreamgeese.drivesafe;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.widget.TextView;
 
 import net.openspatial.GestureEvent;
@@ -10,10 +11,12 @@ public class ViewController {
     public static void setDisplayConnected(Activity currentActivity){
         TextView connection_status = (TextView)currentActivity.findViewById(R.id.connection_status);
         connection_status.setText("Connected");
+        connection_status.setTextColor(Color.parseColor("#27a32d"));
     }
     public static void setDisplayDisconnected(Activity currentActivity){
         TextView connection_status = (TextView)currentActivity.findViewById(R.id.connection_status);
         connection_status.setText("Not Connected");
+        connection_status.setTextColor(Color.parseColor("#e51212"));
     }
 
     public static void showDetectedGestures(Activity currentActivity,GestureEvent gEvent){
