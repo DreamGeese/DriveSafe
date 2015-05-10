aa3d377690ae916bc6651218fee906c0c4e5ca85
Serge Babayan <serj.b.babayan@gmail.com>
2015-05-10 05:14:40 -0400

    Saving usermap to database on first open

--- app/src/main/java/dreamgeese/drivesafe/UserSettings.java
+++ app/src/main/java/dreamgeese/drivesafe/UserSettings.java
@@ -6,7 +6,10 @@ import com.couchbase.lite.Database;
 import com.couchbase.lite.Query;
 import com.couchbase.lite.QueryEnumerator;
 import com.couchbase.lite.QueryRow;
+import com.couchbase.lite.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -28,10 +31,29 @@ public class UserSettings {
 
         try{
             QueryEnumerator result = query.run();
-            for (Iterator<QueryRow> it = result; it.hasNext(); ) {
-                QueryRow row = it.next();
-                Map<String, Object> document=row.getDocumentProperties();
-                Log.e("QUERY",document.toString());
+
+            if(result.getCount()==0){ //if the database is empty (first launch of app)
+                Map<String, Object> properties = new HashMap<String, Object>();
+                properties.put("playMusic", "CLOCKWISE_ROTATION");
+                properties.put("pauseMusic", "COUNTERCLOCKWISE_ROTATION");
+                properties.put("callNumber", "TACTILE1_UP");
+                properties.put("acceptCall", "TOUCH2_UP");
+                properties.put("rejectCall", "TACTILE0_UP");
+                properties.put("raiseVolume", "SWIPE_UP");
+                properties.put("lowerVolume", "SWIPE_DOWN");
+                properties.put("nextSong", "SWIPE_RIGHT");
+                properties.put("previousSong", "SWIPE_LEFT");
+
+                Document document = database.createDocument();
+                document.putProperties(properties);
+                Log.d("dvdsvds",document.getProperties().toString());
+            }
+            else{
+                for (Iterator<QueryRow> it = result; it.hasNext(); ) {
+                    QueryRow row = it.next();
+                    Map<String, Object> document=row.getDocumentProperties();
+                    Log.e("QUERY",document.toString());
+                }
             }
         }catch(Exception e){
             Log.e("QUERY","Could not query the database. Using the default values");
